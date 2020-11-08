d24148a73d8415676d49ef6df669886342805e8d
Zhang Maiyun <myzhang1029@hotmail.com>
2020-11-08 09:32:35 +0800

    Bump version to v0.4.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name="zcbe",
-    version="0.4.3",
+    version="0.4.4",
     description="The Z Cross Build Environment",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
